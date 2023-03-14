08d871da9d2eedf927f243aa474d2036389e1d77
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-03-14 21:23:46 +0100

    resolved task: 2103. Rings and Rods (https://leetcode.com/problems/rings-and-rods/)

---
+++ src/main/java/LeetCode/Algorithms/Easy/Rings_and_Rods/RingsAndRods.java
@@ -0,0 +1,22 @@
+package LeetCode.Algorithms.Easy.Rings_and_Rods;
+
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class RingsAndRods {
+
+    public int countPoints(String rings) {
+
+        Pattern pattern = Pattern.compile("[rgbRGB]\\d");
+        Matcher matcher = pattern.matcher(rings);
+
+        return (int) matcher.results()
+                .map(MatchResult::group)
+                .collect(Collectors.groupingBy(e -> Character.getNumericValue(e.charAt(1)), Collectors.toSet()))
+                .entrySet().stream()
+                .filter(e -> e.getValue().size() == 3)
+                .count();
+    }
+}
