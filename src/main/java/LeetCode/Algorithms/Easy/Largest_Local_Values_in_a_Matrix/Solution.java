6bf73b10eb607f1499593d56096141f01a7dea3f
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-03-17 22:39:44 +0100

    resolved task: 2373. Largest Local Values in a Matrix (https://leetcode.com/problems/largest-local-values-in-a-matrix/)

---
+++ src/main/java/LeetCode/Algorithms/Easy/Largest_Local_Values_in_a_Matrix/Solution.java
@@ -0,0 +1,27 @@
+package LeetCode.Algorithms.Easy.Largest_Local_Values_in_a_Matrix;
+
+public class Solution {
+
+    public int[][] largestLocal(int[][] grid) {
+
+        int n = grid.length - 2;
+        int[][] maxLocal = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int largest = Integer.MIN_VALUE;
+
+                for (int k = i; k < i + 3; k++) {
+                    for (int l = j; l < j + 3; l++) {
+                        if (grid[k][l] > largest) {
+                            largest = grid[k][l];
+                        }
+                    }
+                }
+                maxLocal[i][j] = largest;
+            }
+        }
+        return maxLocal;
+    }
+}
