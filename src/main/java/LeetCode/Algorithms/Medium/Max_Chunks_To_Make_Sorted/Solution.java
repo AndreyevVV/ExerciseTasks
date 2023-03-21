4c2736bc7af1306beadb23c4d39de92b77067faf
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-03-21 23:15:35 +0100

    resolved task: 769. Max Chunks To Make Sorted (https://leetcode.com/problems/max-chunks-to-make-sorted/)

---
+++ src/main/java/LeetCode/Algorithms/Medium/Max_Chunks_To_Make_Sorted/Solution.java
@@ -0,0 +1,19 @@
+package LeetCode.Algorithms.Medium.Max_Chunks_To_Make_Sorted;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int maxChunksToSorted(int[] arr) {
+
+        int length = arr.length;
+        int chunksNumber = 0;
+        int max = 0;
+
+        for (int i = 0; i < length; i++) {
+            max = Math.max(max, arr[i]);
+            if (max == i) chunksNumber++;
+        }
+        return chunksNumber;
+    }
+}
