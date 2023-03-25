af35883a0d8b3e6dabe11ce144a2fe00858ffc1f
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-03-25 21:23:31 +0100

    resolved task: 1. Two Sum (https://leetcode.com/problems/two-sum/)

---
+++ src/main/java/LeetCode/Algorithms/Easy/Two_Sum/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.Algorithms.Easy.Two_Sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int number = target - nums[i];
+
+            if (map.containsKey(number)) {
+                return new int[]{i, map.get(number)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
