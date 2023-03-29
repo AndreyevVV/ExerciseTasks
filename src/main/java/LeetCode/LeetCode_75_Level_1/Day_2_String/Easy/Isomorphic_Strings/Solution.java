03e066b882885e416ec1f5bbc51995462305d9a2
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-03-29 20:26:56 +0200

    refactoring

--- src/main/java/LeetCode/LeetCode_75_Level_1/Day_2_String/Easy/Isomorphic_Strings/Solution.java
+++ src/main/java/LeetCode/LeetCode_75_Level_1/Day_2_String/Easy/Isomorphic_Strings/Solution.java
@@ -4,11 +4,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Solution {
-
-    public static void main(String[] args) {
-        System.out.println(new Solution().isIsomorphic("badc", "baba"));
-    }
-
     public boolean isIsomorphic(String s, String t) {
 
         int sLength = s.length(), tLength = t.length();
