0a01b68c3d23336687a18268edb4a319dc5747e3
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-03-31 21:52:49 +0200

    resolved task: 142. Linked List Cycle II (https://leetcode.com/problems/linked-list-cycle-ii/?envType=study-plan&id=level-1)

---
+++ src/main/java/LeetCode/LeetCode_75_Level_1/Day_4_Linked_List/Medium/Linked_List_Cycle_II/Solution.java
@@ -0,0 +1,39 @@
+package LeetCode.LeetCode_75_Level_1.Day_4_Linked_List.Medium.Linked_List_Cycle_II;
+
+public class Solution {
+
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode turtle = head;
+        ListNode rabbit = head;
+
+        while (rabbit != null && rabbit.next != null) {
+
+            turtle = turtle.next;
+            rabbit = rabbit.next.next;
+
+            if (turtle == rabbit) {
+
+                while (head != rabbit) {
+                    rabbit = rabbit.next;
+                    head = head.next;
+                    int valHead = rabbit.val;
+                    int valRabbit = head.val;
+                    int temp = 0;
+                }
+                return head;
+            }
+        }
+        return null;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
