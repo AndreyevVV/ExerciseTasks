a9c046054b02bc1c308d09f6b4730b1bcf162ec9
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-03 23:23:47 +0200

    resolved task: 704. Binary Search (https://leetcode.com/problems/binary-search/description/?envType=study-plan&id=level-1)

---
+++ src/main/java/LeetCode/LeetCode_75_Level_1/Day_7_Binary_Search/Easy/Binary_Search/Solution.java
@@ -0,0 +1,24 @@
+package LeetCode.LeetCode_75_Level_1.Day_7_Binary_Search.Easy.Binary_Search;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().search(new int[] {5}, 5));
+    }
+
+    public int search(int[] nums, int target) {
+
+        int l = 0, r = nums.length - 1;
+
+        while ( l <= r) {
+
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (target < nums[mid]) r = mid - 1;
+            else l = mid + 1;
+        }
+        return -1;
+    }
+}
