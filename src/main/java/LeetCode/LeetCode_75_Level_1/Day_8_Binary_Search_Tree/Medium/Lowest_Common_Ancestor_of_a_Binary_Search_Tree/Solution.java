9b9fc33f3edc8ba789dc9eca4c9744a7a50f7558
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-05 21:02:53 +0200

    resolved task: 235. Lowest Common Ancestor of a Binary Search Tree (https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/?envType=study-plan&id=level-1)

---
+++ src/main/java/LeetCode/LeetCode_75_Level_1/Day_8_Binary_Search_Tree/Medium/Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.java
@@ -0,0 +1,35 @@
+package LeetCode.LeetCode_75_Level_1.Day_8_Binary_Search_Tree.Medium.Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
+
+public class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        TreeNode LCA = root;
+
+        while (LCA != p || LCA != q) {
+
+            if ( p.val < LCA.val && q.val < LCA.val) {
+                LCA = LCA.left;
+            } else if (p.val > LCA.val && q.val > LCA.val) {
+                LCA = LCA.right;
+            } else return LCA;
+        }
+        return LCA;
+    }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    val + " " +
+                    left.val + " " +
+                    right.val + " " +
+                    "}";
+        }
+    }
+}
