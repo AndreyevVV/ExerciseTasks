f7004f136ccc7daf213830601735a15e49451d0d
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-10 22:50:28 +0200

    resolved task: 62. Unique Paths (https://leetcode.com/problems/unique-paths/description/?envType=study-plan&id=level-1)

---
+++ src/main/java/LeetCode/LeetCode_75_Level_1/Day_11_Dynamic_Programming/Unique_Paths/Solution.java
@@ -0,0 +1,22 @@
+package LeetCode.LeetCode_75_Level_1.Day_11_Dynamic_Programming.Unique_Paths;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution {
+
+    public int uniquePaths(int m, int n) {
+
+        int[][] grid = new int[m][n];
+
+        for (int j = 0; j < n; j++) grid[0][j] = 1;
+        for (int i = 0; i < m; i++) grid[i][0] = 1;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
