c3e57e07db2fa6872a2df94cb64c4598e53bb14e
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-14 22:51:22 +0200

    resolved task: 844. Backspace String Compare (https://leetcode.com/problems/backspace-string-compare/description/?envType=study-plan&id=level-1)

---
+++ src/main/java/LeetCode/LeetCode_75_Level_1/Day_14_Stack/Easy/Solution.java
@@ -0,0 +1,22 @@
+package LeetCode.LeetCode_75_Level_1.Day_14_Stack.Easy;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public boolean backspaceCompare(String s, String t) {
+        return stackFill(s).equals(stackFill(t));
+    }
+
+    private static Stack<Character> stackFill(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char symbol : s.toCharArray()) {
+            if (symbol != '#') {
+                stack.push(symbol);
+            } else if (!stack.isEmpty()) stack.pop();
+        }
+        return stack;
+    }
+}
