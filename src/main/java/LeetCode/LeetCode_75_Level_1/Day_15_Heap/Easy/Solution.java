cb3bbb9da92228b2280454de196946339caf9dc2
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-16 22:53:28 +0200

    resolved task: 1046. Last Stone Weight (https://leetcode.com/problems/last-stone-weight/description/?envType=study-plan&id=level-1?envType=study-plan&id=level-1?envType=study-plan&id=level-1?envType=study-plan&id=level-1)

---
+++ src/main/java/LeetCode/LeetCode_75_Level_1/Day_15_Heap/Easy/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.LeetCode_75_Level_1.Day_15_Heap.Easy;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Solution {
+
+    public int lastStoneWeight(int[] stones) {
+
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int stone: stones) {
+            queue.add(stone);
+        }
+
+        while (queue.size() > 1) {
+            int newStone =  queue.remove() - queue.remove();
+            if (newStone != 0) queue.add(newStone);
+        }
+        return queue.isEmpty() ? 0 : queue.remove();
+    }
+}
