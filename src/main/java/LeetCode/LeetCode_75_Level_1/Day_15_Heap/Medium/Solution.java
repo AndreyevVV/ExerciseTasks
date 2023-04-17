58d51377c82afbfeb5dd2ca1dd44b7e68a69f5e7
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-17 20:20:57 +0200

    resolved task: 692. Top K Frequent Words (https://leetcode.com/problems/top-k-frequent-words/description/?envType=study-plan&id=level-1?envType=study-plan&id=level-1)

---
+++ src/main/java/LeetCode/LeetCode_75_Level_1/Day_15_Heap/Medium/Solution.java
@@ -0,0 +1,36 @@
+package LeetCode.LeetCode_75_Level_1.Day_15_Heap.Medium;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public List<String> topKFrequent(String[] words, int k) {
+
+        Map<String, Integer> freqMap = Arrays.stream(words).collect(Collectors
+                .toMap(Function.identity(), v -> 0, (k1, k2) -> k1, LinkedHashMap::new));
+
+        Arrays.stream(words).forEach(i -> freqMap.computeIfPresent(i, (key, value) -> value + 1));
+
+        Comparator<Map.Entry<String, Integer>> comparator = (entry1, entry2) -> {
+
+            int freqOrder = entry2.getValue() - entry1.getValue();
+
+            if (freqOrder == 0) {
+                return entry1.getKey().compareTo(entry2.getKey());
+            } else return freqOrder;
+        };
+
+        Queue<Map.Entry<String, Integer>> queue = new PriorityQueue<>(comparator);
+
+        queue.addAll(freqMap.entrySet());
+
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 0; i < k; i++) {
+            answer.add(queue.remove().getKey());
+        }
+        return answer;
+    }
+}
