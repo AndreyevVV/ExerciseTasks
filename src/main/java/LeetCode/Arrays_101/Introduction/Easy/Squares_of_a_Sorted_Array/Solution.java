3d860e5292cd0fcad4baa90a28a3a4d9b9d0e88f
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-20 23:29:44 +0200

    refactoring

--- src/main/java/LeetCode/Arrays_101/Introduction/Easy/Squares_of_a_Sorted_Array/Solution.java
+++ src/main/java/LeetCode/Arrays_101/Introduction/Easy/Squares_of_a_Sorted_Array/Solution.java
@@ -1,11 +1,6 @@
 package LeetCode.Arrays_101.Introduction.Easy.Squares_of_a_Sorted_Array;
 
-import java.util.Arrays;
-
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution().sortedSquares(new int[]{-1,1})));
-    }
 
     public int[] sortedSquares(int[] nums) {
         int l = 0, r = nums.length - 1, answerIndex = r;
