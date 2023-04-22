55252bb7d5029f3c8b43ba7c447b4b8c3867b738
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-22 23:10:38 +0200

    refactoring

--- src/main/java/LeetCode/Arrays_101/Inserting_Items_Into_an_Array/Easy/Merge_Sorted_Array/Solution.java
+++ src/main/java/LeetCode/Arrays_101/Inserting_Items_Into_an_Array/Easy/Merge_Sorted_Array/Solution.java
@@ -1,9 +1,6 @@
 package LeetCode.Arrays_101.Inserting_Items_Into_an_Array.Easy.Merge_Sorted_Array;
 
 public class Solution {
-    public static void main(String[] args) {
-        new Solution().merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
-    }
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int nums1Pointer = m - 1, nums2Pointer = n - 1, resultPointer = m + n - 1;
