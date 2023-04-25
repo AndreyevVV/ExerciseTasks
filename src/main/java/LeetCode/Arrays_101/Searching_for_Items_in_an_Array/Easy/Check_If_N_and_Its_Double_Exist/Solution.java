5414be743e4bc09ba1e7c290140274d0f015a527
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-25 23:24:29 +0200

    resolved task: 1346. Check If N and Its Double Exist (https://leetcode.com/problems/check-if-n-and-its-double-exist/description/)

--- src/main/java/LeetCode/Arrays_101/Searching_for_Items_in_an_Array/Easy/Check_If_N_and_Its_Double_Exist/Solution.java
+++ src/main/java/LeetCode/Arrays_101/Searching_for_Items_in_an_Array/Easy/Check_If_N_and_Its_Double_Exist/Solution.java
@@ -3,28 +3,26 @@ package LeetCode.Arrays_101.Searching_for_Items_in_an_Array.Easy.Check_If_N_and_
 import java.util.Arrays;
 
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println(new Solution().checkIfExist(new int[] {10,2,5,3}));
-    }
+
     public boolean checkIfExist(int[] arr) {
         if (arr == null || arr.length < 2) return false;
 
         Arrays.sort(arr);
 
         for (int i = 0; i < arr.length; i++) {
-           int left = 0;
-           int right = arr.length - 1;
+            int left = 0;
+            int right = arr.length - 1;
 
-           while (left <= right) {
-               int mid = left + (right - left) / 2;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
 
-               int target = 2 * arr[i];
+                int target = 2 * arr[i];
 
-               if (arr[mid] == target && mid != i) return true;
+                if (arr[mid] == target && mid != i) return true;
 
-               if (target < arr[mid]) right = mid - 1;
-               else left = mid + 1;
-           }
+                if (target < arr[mid]) right = mid - 1;
+                else left = mid + 1;
+            }
         }
         return false;
     }
