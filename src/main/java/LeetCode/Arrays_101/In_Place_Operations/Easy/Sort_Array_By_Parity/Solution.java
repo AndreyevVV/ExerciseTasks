8cb44c5bb58b425b37588f5796b7a54aac658edb
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-28 23:24:34 +0200

    refactoring

--- src/main/java/LeetCode/Arrays_101/In_Place_Operations/Easy/Sort_Array_By_Parity/Solution.java
+++ src/main/java/LeetCode/Arrays_101/In_Place_Operations/Easy/Sort_Array_By_Parity/Solution.java
@@ -1,11 +1,7 @@
 package LeetCode.Arrays_101.In_Place_Operations.Easy.Sort_Array_By_Parity;
 
-import java.util.Arrays;
-
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution().sortArrayByParity(new int[] {1, 2, 2, 2, 2})));
-    }
+
     public int[] sortArrayByParity(int[] nums) {
         int length = nums.length;
         int evenPointer = length - 1;
