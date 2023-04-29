8c61b1dfaff26a6844638fe174fd18896c44ad5a
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-04-29 23:02:43 +0200

    resolved task: 1051. Height Checker (https://leetcode.com/problems/height-checker/description/)

---
+++ src/main/java/LeetCode/Arrays_101/Conclusion/Easy/Height_Checker/Solution.java
@@ -0,0 +1,17 @@
+package LeetCode.Arrays_101.Conclusion.Easy.Height_Checker;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int heightChecker(int[] heights) {
+        int[] expected = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(expected);
+        int count = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) count++;
+        }
+        return count;
+    }
+}
