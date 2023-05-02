e18b0652b9f9bb6577ea1c8a393fc3b9cb00c000
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-05-02 23:58:13 +0200

    refactoring: 54. Spiral Matrix easy DFS approach (https://leetcode.com/problems/spiral-matrix/description/?envType=study-plan&id=level-2)

--- src/main/java/LeetCode/LeetCode_75_Level_2/Day_1_Implementation_Simulation/Day_2_String/Medium/Spiral_Matrix/Solution.java
+++ src/main/java/LeetCode/LeetCode_75_Level_2/Day_1_Implementation_Simulation/Day_2_String/Medium/Spiral_Matrix/Solution.java
@@ -1,37 +1,30 @@
 package LeetCode.LeetCode_75_Level_2.Day_1_Implementation_Simulation.Day_2_String.Medium.Spiral_Matrix;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Solution {
 
     public List<Integer> spiralOrder(int[][] matrix) {
-        Set<Integer> visited = new HashSet<>();
+        int color = 101;
         List<Integer> list = new ArrayList<>();
 
-        dfs(matrix, 0, 0, list, visited);
-
+        dfs(matrix, 0, 0, list, color);
         return list;
     }
 
-    public void dfs(int[][] matrix, int sr, int sc, List<Integer> list, Set<Integer> visited) {
-
-        if (sr < 0 || sc < 0 || sr >= matrix.length || sc >= matrix[0].length) return;
-
-        int index = (sr * matrix[sr].length) + sc;
-        if (visited.contains(index)) return;
+    public void dfs(int[][] matrix, int sr, int sc, List<Integer> list, int color) {
+        if (sr < 0 || sc < 0 || sr >= matrix.length || sc >= matrix[0].length || matrix[sr][sc] == color) return;
 
         list.add(matrix[sr][sc]);
-        visited.add(index);
+        matrix[sr][sc] = color;
 
-        if (sr - 1 < 0 || visited.contains(((sr - 1) * matrix[sr - 1].length) + sc)) {
-            dfs(matrix, sr, sc + 1, list, visited);
+        if (sr - 1 < 0 || matrix[sr - 1][sc] == color) {
+            dfs(matrix, sr, sc + 1, list, color);
         }
 
-        dfs(matrix, sr + 1, sc, list, visited);
-        dfs(matrix, sr, sc - 1, list, visited);
-        dfs(matrix, sr - 1, sc, list, visited);
+        dfs(matrix, sr + 1, sc, list, color);
+        dfs(matrix, sr, sc - 1, list, color);
+        dfs(matrix, sr - 1, sc, list, color);
     }
 }
