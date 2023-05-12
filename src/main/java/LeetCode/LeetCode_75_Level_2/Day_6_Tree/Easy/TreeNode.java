c58fa073f160f17f4a53dc7cfe871627a9c10608
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-05-12 22:58:36 +0200

    refactoring

---
+++ src/main/java/LeetCode/LeetCode_75_Level_2/Day_6_Tree/Easy/TreeNode.java
@@ -0,0 +1,29 @@
+package LeetCode.LeetCode_75_Level_2.Day_6_Tree.Easy;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "" + val +
+                " " + left +
+                " " + right +
+                '}';
+    }
+}
