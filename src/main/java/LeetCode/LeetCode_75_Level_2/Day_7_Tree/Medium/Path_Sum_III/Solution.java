d3ffcc9feef2fd8ddf50f060989a5592ac0b61f4
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-05-15 23:15:22 +0200

    resolved task: 437. Path Sum III (https://leetcode.com/problems/path-sum-iii/?envType=study-plan&id=level-2) with frequency map

---
+++ src/main/java/LeetCode/LeetCode_75_Level_2/Day_7_Tree/Medium/Path_Sum_III/Solution.java
@@ -0,0 +1,59 @@
+package LeetCode.LeetCode_75_Level_2.Day_7_Tree.Medium.Path_Sum_III;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        return findSum(root, 0, targetSum, map);
+    }
+
+    public int findSum(TreeNode root, int sum, int target, Map<Integer, Integer> map) {
+        if (root == null) return 0;
+
+        sum += root.val;
+        int rootsCount = 0;
+
+        rootsCount += map.getOrDefault(sum - target, 0);
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
+
+        rootsCount += findSum(root.left, sum, target, map);
+        rootsCount += findSum(root.right, sum, target, map);
+
+        map.put(sum, map.get(sum) - 1);
+        return rootsCount;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "" + val +
+                    " " + left +
+                    " " + right +
+                    '}';
+        }
+    }
+}
