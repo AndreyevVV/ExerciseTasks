c01267f6c5090d2427e9f32479f8099d9eff671a
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-05-18 23:47:28 +0200

    replaced Main class threads creation with executorService

--- src/main/java/LeetCode/Concurrency/Medium/Print_Zero_Even_Odd/Main.java
+++ src/main/java/LeetCode/Concurrency/Medium/Print_Zero_Even_Odd/Main.java
@@ -3,48 +3,64 @@ package LeetCode.Concurrency.Medium.Print_Zero_Even_Odd;
 import LeetCode.Concurrency.Medium.Print_Zero_Even_Odd.Semaphore_solution.ZeroEvenOdd;
 //import LeetCode.Concurrency.Medium.Print_Zero_Even_Odd.Synchronized_on_Object_solution.ZeroEvenOdd;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.IntConsumer;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 10; // Задайте нужное значение n
+        int n = 10;
 
         ZeroEvenOdd zeroEvenOdd = new ZeroEvenOdd(n);
 
-        IntConsumer printNumber = System.out::print; // Реализация функции printNumber, которая просто выводит число в консоль
+        IntConsumer printNumber = System.out::print;
 
-        Thread threadA = new Thread(() -> {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        AtomicInteger count = new AtomicInteger(0);
+
+        executorService.execute(() -> {
             try {
-                zeroEvenOdd.zero(printNumber);
+                zeroEvenOdd.zero(number -> {
+                    if (count.incrementAndGet() <= 2 * n) {
+                        printNumber.accept(number);
+                    }
+                });
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
 
-        Thread threadB = new Thread(() -> {
+        executorService.execute(() -> {
             try {
-                zeroEvenOdd.even(printNumber);
+                zeroEvenOdd.even(number -> {
+                    if (count.incrementAndGet() <= 2 * n) {
+                        printNumber.accept(number);
+                    }
+                });
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
 
-        Thread threadC = new Thread(() -> {
+        executorService.execute(() -> {
             try {
-                zeroEvenOdd.odd(printNumber);
+                zeroEvenOdd.odd(number -> {
+                    if (count.incrementAndGet() <= 2 * n) {
+                        printNumber.accept(number);
+                    }
+                });
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
 
-        threadA.start();
-        threadB.start();
-        threadC.start();
+        executorService.shutdown();
 
         try {
-            threadA.join();
-            threadB.join();
-            threadC.join();
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
