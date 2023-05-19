80a4e785305439e82763bafce598fdf3f98d4d4e
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-05-19 22:49:41 +0200

    task: 1115. Print FooBar Alternately (https://leetcode.com/problems/print-foobar-alternately/description/) added ReentrantLock solution

---
+++ src/main/java/LeetCode/Concurrency/Medium/Print_FooBar_Alternately/ReentrantLock_solution/FooBar.java
@@ -0,0 +1,49 @@
+package LeetCode.Concurrency.Medium.Print_FooBar_Alternately.ReentrantLock_solution;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FooBar {
+    private int n;
+    private ReentrantLock mutex = new ReentrantLock();
+    private Condition condition;
+
+    private AtomicBoolean flag;
+
+    public FooBar(int n) {
+        this.n = n;
+        mutex = new ReentrantLock();
+        condition = mutex.newCondition();
+        flag = new AtomicBoolean(true);
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            try {
+                mutex.lock();
+                while (!flag.get()) condition.await();
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                flag.getAndSet(false);
+                condition.signalAll();
+            } finally {
+                mutex.unlock();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        try {
+            mutex.lock();
+            while (flag.get() == true) condition.await();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            flag.getAndSet(true);
+            condition.signalAll();
+        } finally {
+            mutex.unlock();
+        }
+    }
+}
