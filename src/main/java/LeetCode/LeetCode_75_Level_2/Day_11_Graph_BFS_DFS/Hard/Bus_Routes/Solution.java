4c35daea630e5110a6a3b3b5c7c16769c43a4986
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-05-30 22:53:19 +0200

    resolved task: 815. Bus Routes (https://leetcode.com/problems/bus-routes/description/)

---
+++ src/main/java/LeetCode/LeetCode_75_Level_2/Day_11_Graph_BFS_DFS/Hard/Bus_Routes/Solution.java
@@ -0,0 +1,41 @@
+package LeetCode.LeetCode_75_Level_2.Day_11_Graph_BFS_DFS.Hard.Bus_Routes;
+
+import java.util.*;
+
+public class Solution {
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        if (source == target) return 0;
+
+        HashSet<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        int answer = 0;
+
+        for (int i = 0; i < routes.length; i++) {
+            for (int j = 0; j < routes[i].length; j++) {
+                map.computeIfAbsent(routes[i][j], key -> new ArrayList<>()).add(i);
+            }
+        }
+
+        queue.offer(source);
+
+        while (!queue.isEmpty()) {
+            int length = queue.size();
+            answer++;
+
+            for (int i = 0; i < length; i++) {
+                int currentNode = queue.poll();
+                ArrayList<Integer> listOfAdjacentVertices = map.get(currentNode);
+                for (int node : listOfAdjacentVertices) {
+                    if (visited.contains(node)) continue;
+                    visited.add(node);
+                    for (int j = 0; j < routes[node].length; j++) {
+                        if (routes[node][j] == target) return answer;
+                        queue.offer(routes[node][j]);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
