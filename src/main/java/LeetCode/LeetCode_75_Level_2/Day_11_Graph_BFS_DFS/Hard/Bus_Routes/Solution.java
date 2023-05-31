2ef75be1e9d4ffcb2d77e9e2562f62e9c2279bd7
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-05-31 21:08:26 +0200

    refactoring

--- src/main/java/LeetCode/LeetCode_75_Level_2/Day_11_Graph_BFS_DFS/Hard/Bus_Routes/Solution.java
+++ src/main/java/LeetCode/LeetCode_75_Level_2/Day_11_Graph_BFS_DFS/Hard/Bus_Routes/Solution.java
@@ -3,10 +3,15 @@ package LeetCode.LeetCode_75_Level_2.Day_11_Graph_BFS_DFS.Hard.Bus_Routes;
 import java.util.*;
 
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().numBusesToDestination(
+                new int[][]{{7, 12}, {4, 5, 15}, {6}, {15, 19}, {9, 12, 13}}, 15, 12));
+    }
+
     public int numBusesToDestination(int[][] routes, int source, int target) {
         if (source == target) return 0;
 
-        HashSet<Integer> visited = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
         Queue<Integer> queue = new LinkedList<>();
         Map<Integer, ArrayList<Integer>> map = new HashMap<>();
         int answer = 0;
