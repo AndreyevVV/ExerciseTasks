d4da540a1f06c288b8ea1f5c9d58b6a2779f132a
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-06-16 22:47:18 +0200

    refactoring

--- src/main/java/LeetCode/LeetCode_75_Level_2/Day_18_Stack/Medium/Asteroid_Collision/Solution.java
+++ src/main/java/LeetCode/LeetCode_75_Level_2/Day_18_Stack/Medium/Asteroid_Collision/Solution.java
@@ -5,19 +5,20 @@ import java.util.Stack;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution().asteroidCollision(new int[]{8, -8})));
+        System.out.println(Arrays.toString(new Solution().asteroidCollision(new int[]{-2, -1, 1, 2})));
     }
 
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
 
         for (int asteroid : asteroids) {
-            if (asteroid <= 0) {
+            if (asteroid > 0 || stack.isEmpty()) {
+                stack.push(asteroid);
+            } else {
                 while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroid)) stack.pop();
-                if (stack.peek() == Math.abs(asteroid)) stack.pop();
-                else if (!stack.isEmpty() && stack.peek() >= Math.abs(asteroid)) continue;
-                else stack.push(asteroid);
-            } else stack.push(asteroid);
+                if (!stack.isEmpty() && stack.peek() == Math.abs(asteroid)) stack.pop();
+                else if (stack.isEmpty() || stack.peek() < 0) stack.push(asteroid);
+            }
         }
         return stack.stream().mapToInt(Integer::intValue).toArray();
     }
