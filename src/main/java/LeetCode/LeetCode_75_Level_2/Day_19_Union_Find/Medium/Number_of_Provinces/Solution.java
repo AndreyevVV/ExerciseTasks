468957ac0894d9d5c15255be75a5d2e6eff0dc6a
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-06-18 23:10:21 +0200

    resolved task: 547. Number of Provinces (https://leetcode.com/problems/number-of-provinces/description/?envType=study-plan&envId=level-2&plan=leetcode-75)

---
+++ src/main/java/LeetCode/LeetCode_75_Level_2/Day_19_Union_Find/Medium/Number_of_Provinces/Solution.java
@@ -0,0 +1,44 @@
+package LeetCode.LeetCode_75_Level_2.Day_19_Union_Find.Medium.Number_of_Provinces;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+
+    public int findCircleNum(int[][] isConnected) {
+        int length = isConnected.length, answer = 0;
+        boolean[] visited = new boolean[length];
+
+        for (int i = 0; i < length; i++) {
+            if (!visited[i]) {
+                dfs(i, isConnected, visited); // bfs(i, isConnected, visited);
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    private void dfs(int from, int[][] isConnected, boolean[] visited) {
+        visited[from] = true;
+        for (int i = 0; i < isConnected[from].length; i++) {
+            if (!visited[i] && isConnected[from][i] == 1) {
+                dfs(i, isConnected, visited);
+            }
+        }
+    }
+
+    private void bfs(int from, int[][] isConnected, boolean[] visited) {
+        visited[from] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(from);
+        while (!queue.isEmpty()) {
+            int j = queue.poll();
+            for (int i = 0; i < isConnected[j].length; i++) {
+                if (!visited[i] && isConnected[j][i] == 1) {
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+}
