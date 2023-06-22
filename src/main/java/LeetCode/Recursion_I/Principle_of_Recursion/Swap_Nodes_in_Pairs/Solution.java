dca282c1da0b4e3eddef0afbdd418b909be80953
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-06-22 23:03:49 +0200

    refactoring

--- src/main/java/LeetCode/Recursion_I/Principle_of_Recursion/Swap_Nodes_in_Pairs/Solution.java
+++ src/main/java/LeetCode/Recursion_I/Principle_of_Recursion/Swap_Nodes_in_Pairs/Solution.java
@@ -1,6 +1,20 @@
 package LeetCode.Recursion_I.Principle_of_Recursion.Swap_Nodes_in_Pairs;
 
+import java.util.List;
+
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().start());
+    }
+
+    private ListNode start() {
+
+        ListNode four = new ListNode(4);
+        ListNode three = new ListNode(3, four);
+        ListNode two = new ListNode(2, three);
+        ListNode head = new ListNode(1, two);
+        return swapPairs(head);
+    }
 
     public ListNode swapPairs(ListNode head) {
         if ((head == null) || (head.next == null)) return head;
