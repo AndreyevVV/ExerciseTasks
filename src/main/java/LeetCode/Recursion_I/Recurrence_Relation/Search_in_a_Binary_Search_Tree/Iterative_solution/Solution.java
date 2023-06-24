9b11ae95cd428d93d1077267af20ee9876002a9e
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-06-24 13:41:10 +0200

    resolved task: 700. Search in a Binary Search Tree (https://leetcode.com/problems/search-in-a-binary-search-tree/description/)

---
+++ src/main/java/LeetCode/Recursion_I/Recurrence_Relation/Search_in_a_Binary_Search_Tree/Iterative_solution/Solution.java
@@ -0,0 +1,53 @@
+package LeetCode.Recursion_I.Recurrence_Relation.Search_in_a_Binary_Search_Tree.Iterative_solution;
+
+import java.util.Stack;
+
+public class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+
+            if (root.val == val) return root;
+
+            root = root.right;
+        }
+
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+}
