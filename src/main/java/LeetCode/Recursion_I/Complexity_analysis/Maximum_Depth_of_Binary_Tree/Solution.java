d75cefe0345671985d602281951128d709f5e523
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-06-27 23:30:45 +0200

    resolved task: 104. Maximum Depth of Binary Tree (https://leetcode.com/problems/maximum-depth-of-binary-tree/description/)

---
+++ src/main/java/LeetCode/Recursion_I/Complexity_analysis/Maximum_Depth_of_Binary_Tree/Solution.java
@@ -0,0 +1,39 @@
+package LeetCode.Recursion_I.Complexity_analysis.Maximum_Depth_of_Binary_Tree;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+}
