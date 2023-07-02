2818262078d0bb270722b456c3adcf222abc269d
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-07-02 23:22:46 +0200

    resolved task: 1768. Merge Strings Alternately (https://leetcode.com/problems/merge-strings-alternately/description/)

---
+++ src/main/java/LeetCode/LeetCode_75/Array_String/Merge_Strings_Alternately/Solution.java
@@ -0,0 +1,16 @@
+package LeetCode.LeetCode_75.Array_String.Merge_Strings_Alternately;
+
+public class Solution {
+
+    public String mergeAlternately(String word, String word2) {
+        StringBuilder answer = new StringBuilder();
+        int length1 = word.length(), length2 = word2.length();
+        int i = 0, j = 0;
+
+        while (i < length1 || j < length2) {
+            if(i<length1) answer.append(word.charAt(i++));
+            if(j<length2) answer.append(word2.charAt(j++));
+        }
+        return answer.toString();
+    }
+}
