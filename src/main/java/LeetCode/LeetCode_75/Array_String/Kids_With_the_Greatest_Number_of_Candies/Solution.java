b15d5d875e3cca940b3959f637945c2d1ee2b542
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-07-04 22:28:22 +0200

    resolved task: 1431. Kids With the Greatest Number of Candies (https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/)

---
+++ src/main/java/LeetCode/LeetCode_75/Array_String/Kids_With_the_Greatest_Number_of_Candies/Solution.java
@@ -0,0 +1,20 @@
+package LeetCode.LeetCode_75.Array_String.Kids_With_the_Greatest_Number_of_Candies;
+
+import java.util.*;
+
+public class Solution {
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        if (candies.length <= 1) return List.of(true);
+        int maxValue = Arrays.stream(candies).max().getAsInt();
+
+        int criterion = maxValue - extraCandies;
+        List<Boolean> answer = new ArrayList<>();
+
+        for (int candiesPack : candies) {
+            if (candiesPack >= criterion) answer.add(true);
+            else answer.add(false);
+        }
+        return answer;
+    }
+}
