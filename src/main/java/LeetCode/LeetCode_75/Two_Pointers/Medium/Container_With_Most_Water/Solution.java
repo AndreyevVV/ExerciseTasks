62f6f0949bb45827e61fa60f670ab71ca056fa5f
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-07-12 23:39:56 +0200

    resolved task: 11. Container With Most Water (https://leetcode.com/problems/container-with-most-water/description/)

---
+++ src/main/java/LeetCode/LeetCode_75/Two_Pointers/Medium/Container_With_Most_Water/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.LeetCode_75.Two_Pointers.Medium.Container_With_Most_Water;
+
+public class Solution {
+
+    public int maxArea(int[] height) {
+        if (height == null || height.length < 2) return 0;
+
+        int leftBorderIndex = 0, rightBorderIndex = height.length - 1;
+        int maxArea = (rightBorderIndex - leftBorderIndex)
+                * Math.min(height[leftBorderIndex], height[rightBorderIndex]);
+
+        while (leftBorderIndex < rightBorderIndex) {
+            int bottom = leftBorderIndex - rightBorderIndex;
+            int leftBorder = height[leftBorderIndex], rightBorder = height[rightBorderIndex];
+
+            maxArea = Math.max(maxArea, bottom * Math.min(leftBorder, rightBorder));
+
+            if (height[leftBorderIndex] <= height[rightBorderIndex]) leftBorderIndex++;
+            else rightBorderIndex--;
+        }
+        return maxArea;
+    }
+}
