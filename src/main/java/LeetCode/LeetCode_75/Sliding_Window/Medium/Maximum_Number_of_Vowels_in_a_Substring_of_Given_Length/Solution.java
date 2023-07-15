174b63734629110405bcf3b29063ca4fbc13e692
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-07-15 22:38:10 +0200

    resolved task: 1456. Maximum Number of Vowels in a Substring of Given Length (https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/?envType=study-plan-v2&envId=leetcode-75)

---
+++ src/main/java/LeetCode/LeetCode_75/Sliding_Window/Medium/Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/Solution.java
@@ -0,0 +1,29 @@
+package LeetCode.LeetCode_75.Sliding_Window.Medium.Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().maxVowels("leetcode", 3));
+    }
+
+    public int maxVowels(String s, int k) {
+        if (s == null || s.length() == 0) return 0;
+        Set<Character> vowelsSet = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+        int vowelsCount = 0, answer = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < k)
+                vowelsCount += vowelsSet.contains((s.charAt(i))) ? 1 : 0;
+            else {
+                vowelsCount += vowelsSet.contains((s.charAt(i))) ? 1 : 0;
+                vowelsCount -= vowelsSet.contains((s.charAt(i - k))) ? 1 : 0;
+            }
+            answer = Math.max(answer, vowelsCount);
+            if (answer == k) return k;
+        }
+        return answer;
+    }
+}
