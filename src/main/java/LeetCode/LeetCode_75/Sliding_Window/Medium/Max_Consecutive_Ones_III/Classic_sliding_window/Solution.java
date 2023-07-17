c751348daba232ce6a6d60c1b914c3607b995756
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-07-17 22:04:01 +0200

    refactoring

--- src/main/java/LeetCode/LeetCode_75/Sliding_Window/Medium/Max_Consecutive_Ones_III/Classic_sliding_window/Solution.java
+++ src/main/java/LeetCode/LeetCode_75/Sliding_Window/Medium/Max_Consecutive_Ones_III/Classic_sliding_window/Solution.java
@@ -1,6 +1,9 @@
 package LeetCode.LeetCode_75.Sliding_Window.Medium.Max_Consecutive_Ones_III.Classic_sliding_window;
 
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().longestOnes(new int[] {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1}, 3));
+    }
 
     public int longestOnes(int[] nums, int k) {
         int left = 0;
