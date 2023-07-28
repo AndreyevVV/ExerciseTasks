0276f2a8c9f4f7435dcfc5aea7abce1d1025b168
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-07-28 19:36:49 +0200

    refactoring

--- src/main/java/LeetCode/Recursion_I/Recurrence_Relation/Reverse_Linked_List/Solution.java
+++ src/main/java/LeetCode/Recursion_I/Recurrence_Relation/Reverse_Linked_List/Solution.java
@@ -1,6 +1,22 @@
 package LeetCode.Recursion_I.Recurrence_Relation.Reverse_Linked_List;
 
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().start());
+    }
+
+    private ListNode start() {
+
+        ListNode six = new ListNode(6, null);
+        ListNode five = new ListNode(5, six);
+        ListNode four = new ListNode(4, five);
+        ListNode three = new ListNode(3, four);
+        ListNode two = new ListNode(2, three);
+        ListNode one = new ListNode(1, two);
+        ListNode head = new ListNode(0, one);
+
+        return reverseList(head);
+    }
 
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) return head;
@@ -28,6 +44,11 @@ public class Solution {
         ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s %s", val, next);
         }
     }
 }
