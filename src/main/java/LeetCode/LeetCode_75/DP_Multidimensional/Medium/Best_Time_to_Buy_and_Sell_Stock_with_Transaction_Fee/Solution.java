eafb1621b23927c2c33e6177c590d96b613cb552
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-08-24 23:39:41 +0200

    resolved task: 714. Best Time to Buy and Sell Stock with Transaction Fee (https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/?envType=study-plan-v2&envId=leetcode-75)

---
+++ src/main/java/LeetCode/LeetCode_75/DP_Multidimensional/Medium/Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee/Solution.java
@@ -0,0 +1,18 @@
+package LeetCode.LeetCode_75.DP_Multidimensional.Medium.Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee;
+
+public class Solution {
+
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+}
