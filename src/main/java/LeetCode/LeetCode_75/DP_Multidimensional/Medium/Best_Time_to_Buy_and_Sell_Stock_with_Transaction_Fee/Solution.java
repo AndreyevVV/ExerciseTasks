c97659b9681805db85f35819ba133d8e3b0f21e5
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-08-25 22:46:55 +0200

    refactoring

--- src/main/java/LeetCode/LeetCode_75/DP_Multidimensional/Medium/Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee/Solution.java
+++ src/main/java/LeetCode/LeetCode_75/DP_Multidimensional/Medium/Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee/Solution.java
@@ -1,6 +1,11 @@
 package LeetCode.LeetCode_75.DP_Multidimensional.Medium.Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee;
 
+import java.util.Arrays;
+
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+    }
 
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
@@ -13,6 +18,7 @@ public class Solution {
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
             dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
         }
+        System.out.println(Arrays.deepToString(dp));
         return dp[n - 1][0];
     }
 }
