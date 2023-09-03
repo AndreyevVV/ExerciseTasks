2d693b9804983b6bcc0c9a5fa9733a9640c57f31
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-09-03 23:33:58 +0200

    resolved task: 78. Subsets (https://leetcode.com/problems/subsets/description/) with Recursion approach

---
+++ src/main/java/LeetCode/General_approaches_to_backtracking_questions/Subsets/Subsets/Recursion_solution/Solution.java
@@ -0,0 +1,24 @@
+package LeetCode.General_approaches_to_backtracking_questions.Subsets.Subsets.Recursion_solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
