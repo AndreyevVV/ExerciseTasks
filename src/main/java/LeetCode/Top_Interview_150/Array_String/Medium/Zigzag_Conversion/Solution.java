12ba8921d47ed49489d612486a402983b5785d21
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-09-19 23:30:43 +0200

    resolved task: 6. Zigzag Conversion (https://leetcode.com/problems/zigzag-conversion/description/?envType=study-plan-v2&envId=top-interview-150)

---
+++ src/main/java/LeetCode/Top_Interview_150/Array_String/Medium/Zigzag_Conversion/Solution.java
@@ -0,0 +1,32 @@
+package LeetCode.Top_Interview_150.Array_String.Medium.Zigzag_Conversion;
+
+public class Solution {
+
+    public String convert(String s, int numRows) {
+        if (numRows <= 1 || s.length() <= numRows) {
+            return s;
+        }
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int currentRow = 0;
+        boolean stepDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows[currentRow].append(c);
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                stepDown = !stepDown;
+            }
+            currentRow += stepDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+        return result.toString();
+    }
+}
