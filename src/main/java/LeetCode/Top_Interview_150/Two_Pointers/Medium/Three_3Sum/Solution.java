b100eb5617b9079c32d363b55c6a932466a6967e
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-09-22 23:48:00 +0200

    resolved task: 15. 3Sum (https://leetcode.com/problems/3sum/description/?envType=study-plan-v2&envId=top-interview-150)

---
+++ src/main/java/LeetCode/Top_Interview_150/Two_Pointers/Medium/Three_3Sum/Solution.java
@@ -0,0 +1,47 @@
+package LeetCode.Top_Interview_150.Two_Pointers.Medium.Three_3Sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return answer;
+    }
+}
