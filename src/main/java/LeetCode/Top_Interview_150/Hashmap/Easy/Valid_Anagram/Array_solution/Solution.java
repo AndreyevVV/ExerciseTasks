7c62f5cd65e3960c2e255411f8479181738185b2
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-10-01 23:20:37 +0200

    refactoring

--- src/main/java/LeetCode/Top_Interview_150/Hashmap/Easy/Valid_Anagram/Array_solution/Solution.java
+++ src/main/java/LeetCode/Top_Interview_150/Hashmap/Easy/Valid_Anagram/Array_solution/Solution.java
@@ -1,19 +1,20 @@
 package LeetCode.Top_Interview_150.Hashmap.Easy.Valid_Anagram.Array_solution;
 
-import java.util.Arrays;
-
 public class Solution {
 
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
 
         int[] sMap = new int[26];
-        int[] tMap = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
             sMap[s.charAt(i) - 'a']++;
-            tMap[t.charAt(i) - 'a']++;
+            sMap[t.charAt(i) - 'a']--;
         }
-        return Arrays.equals(sMap, tMap);
+
+        for (int i = 0; i < 26; i++) {
+            if (sMap[i] != 0) return false;
+        }
+        return true;
     }
 }
