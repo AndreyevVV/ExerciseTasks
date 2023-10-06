91ae868258446586588500d8fd8659b9cfde5204
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-10-06 23:07:09 +0200

    resolved task: 20. Valid Parentheses (https://leetcode.com/problems/valid-parentheses/description/?envType=study-plan-v2&envId=top-interview-150)

---
+++ src/main/java/LeetCode/Top_Interview_150/Stack/Valid_Parentheses/Solution.java
@@ -0,0 +1,25 @@
+package LeetCode.Top_Interview_150.Stack.Valid_Parentheses;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') stack.push(c);
+            else {
+                if (stack.isEmpty()) return false;
+
+                Character character = stack.pop();
+
+                if ((c == ')' && character != '(') ||
+                        (c == '}' && character != '{') ||
+                        (c == ']' && character != '[')) return false;
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
