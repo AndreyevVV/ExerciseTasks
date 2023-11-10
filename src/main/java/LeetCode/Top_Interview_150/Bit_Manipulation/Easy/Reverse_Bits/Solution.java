dc3c9e03a9260dd5047846f81750535b478ae4ce
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-11-10 22:47:19 +0100

    resolved task: 190. Reverse Bits (https://leetcode.com/problems/reverse-bits/description/?envType=study-plan-v2&envId=top-interview-150)

---
+++ src/main/java/LeetCode/Top_Interview_150/Bit_Manipulation/Easy/Reverse_Bits/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.Top_Interview_150.Bit_Manipulation.Easy.Reverse_Bits;
+
+public class Solution {
+
+    // you need treat n as an unsigned value
+    public static int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result = (result << 1) | (n & 1);
+            n >>= 1;
+        }
+        return result;
+    }
+}
