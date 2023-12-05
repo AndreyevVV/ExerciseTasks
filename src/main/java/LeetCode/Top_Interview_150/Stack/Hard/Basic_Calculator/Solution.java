9d40111b62cc2e2befc742282a9ff6814b2a428c
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-12-05 23:07:55 +0100

    resolved task: 224. Basic Calculator (https://leetcode.com/problems/basic-calculator/?envType=study-plan-v2&envId=top-interview-150) with using stack approach

---
+++ src/main/java/LeetCode/Top_Interview_150/Stack/Hard/Basic_Calculator/Solution.java
@@ -0,0 +1,43 @@
+package LeetCode.Top_Interview_150.Stack.Hard.Basic_Calculator;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        int sign = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                int num = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                i--;
+
+                result += sign * num;
+            } else if (ch == '+') {
+                sign = 1;
+            } else if (ch == '-') {
+                sign = -1;
+            } else if (ch == '(') {
+                stack.push(result);
+                stack.push(sign);
+
+                result = 0;
+                sign = 1;
+            } else if (ch == ')') {
+                int prevSign = stack.pop();
+                int prevResult = stack.pop();
+
+                result = prevResult + prevSign * result;
+            }
+        }
+        return result;
+    }
+}
