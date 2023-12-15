68136ea0ce003ea444ea47cf51c8e7dac01dfbbf
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-12-15 21:03:43 +0100

    resolved task: 139. Word Break (https://leetcode.com/problems/word-break/description/?envType=study-plan-v2&envId=top-interview-150) with DP approach

---
+++ src/main/java/LeetCode/Top_Interview_150/OneD_DP/Medium/Word_Break/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.Top_Interview_150.OneD_DP.Medium.Word_Break;
+
+import java.util.List;
+
+public class Solution {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (String word : wordDict) {
+                int len = word.length();
+                if (i >= len && dp[i - len] && s.substring(i - len, i).equals(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
