2624365dabfb65b61df412832357f5b1a2ce9df5
Andreev Vladimir <andreyevpostbox@gmail.com>
2023-12-17 20:50:27 +0100

    refactoring

--- src/main/java/LeetCode/Top_Interview_150/Heap/Medium/Find_K_Pairs_with_Smallest_Sums/Solution.java
+++ src/main/java/LeetCode/Top_Interview_150/Heap/Medium/Find_K_Pairs_with_Smallest_Sums/Solution.java
@@ -7,30 +7,33 @@ import java.util.PriorityQueue;
 public class Solution {
 
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        List<List<Integer>> result = new ArrayList<>();
 
-        if (nums1.length == 0 || nums2.length == 0 || k == 0)
-            return result;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (b[0] + b[1]) - (a[0] + a[1]));
 
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] + a[1] - b[0] - b[1]);
+        for (int num1 : nums1) {
+            for (int num2 : nums2) {
+                minHeap.offer(new int[]{num1, num2});
+                if (minHeap.size() > k) {
+                    minHeap.poll();
 
-        for (int i = 0; i < nums1.length && i < k; i++)
-            minHeap.offer(new int[]{nums1[i], nums2[0], 0});
-
-        while (k-- > 0 && !minHeap.isEmpty()) {
-            int[] current = minHeap.poll();
-            int num1 = current[0];
-            int num2 = current[1];
-            int index = current[2];
-
-            List<Integer> pair = new ArrayList<>();
-            pair.add(num1);
-            pair.add(num2);
-            result.add(pair);
-
-            if (index + 1 < nums2.length)
-                minHeap.offer(new int[]{num1, nums2[index + 1], index + 1});
+                    int currentSum = num1 + num2;
+                    int[] maxSumPair = minHeap.peek();
+                    if (currentSum >= (maxSumPair[0] + maxSumPair[1])) {
+                        break;
+                    }
+                }
+            }
         }
-        return result;
+
+        List<List<Integer>> kPairs = new ArrayList<>();
+        int heapSize = minHeap.size();
+        for (int i = 0; i < heapSize; i++) {
+            int[] pairArr = minHeap.poll();
+            List<Integer> pair = new ArrayList<>();
+            pair.add(pairArr[0]);
+            pair.add(pairArr[1]);
+            kPairs.add(pair);
+        }
+        return kPairs;
     }
 }
