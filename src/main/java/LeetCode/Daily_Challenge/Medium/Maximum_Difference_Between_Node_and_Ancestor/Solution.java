650c6ee9ef39b33b7f42db9d630179695037f1ab
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-01-11 20:54:12 +0100

    resolved task:1026. Maximum Difference Between Node and Ancestor (https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/description/?envType=daily-question&envId=2024-01-11)

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Maximum_Difference_Between_Node_and_Ancestor/Solution.java
@@ -0,0 +1,40 @@
+package LeetCode.Daily_Challenge.Medium.Maximum_Difference_Between_Node_and_Ancestor;
+
+public class Solution {
+
+    public int maxAncestorDiff(TreeNode root) {
+        return helper(root, root.val, root.val);
+    }
+
+    private int helper(TreeNode node, int min, int max) {
+        if (node == null)
+            return max - min;
+
+        min = Math.min(min, node.val);
+        max = Math.max(max, node.val);
+
+        int leftDiff = helper(node.left, min, max);
+        int rightDiff = helper(node.right, min, max);
+
+        return Math.max(leftDiff, rightDiff);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
