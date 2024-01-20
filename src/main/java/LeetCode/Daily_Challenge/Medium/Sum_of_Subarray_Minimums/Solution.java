6aac58fe950d9b6160c657a46cfb72b262031229
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-01-20 21:08:34 +0300

    resolved task: 907. Sum of Subarray Minimums (https://leetcode.com/problems/sum-of-subarray-minimums/description/?envType=daily-question&envId=2024-01-20) with prefix sum approach and stack

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Sum_of_Subarray_Minimums/Solution.java
@@ -0,0 +1,36 @@
+package LeetCode.Daily_Challenge.Medium.Sum_of_Subarray_Minimums;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public int sumSubarrayMins(int[] arr) {
+        int MOD = 1000000007;
+        int n = arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[i] < arr[stack.peek()])
+                stack.pop();
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+
+        stack.clear();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[i] <= arr[stack.peek()])
+                stack.pop();
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+
+        long result = 0;
+        for (int i = 0; i < n; i++)
+            result = (result + (long) arr[i] * (i - left[i]) * (right[i] - i)) % MOD;
+
+        return (int) result;
+    }
+}
