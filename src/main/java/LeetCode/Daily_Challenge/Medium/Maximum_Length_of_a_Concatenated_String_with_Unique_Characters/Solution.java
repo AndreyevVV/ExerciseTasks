11490d92a6dffd72c0be13d72274def84aeb4a76
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-01-23 23:20:13 +0300

    resolved task: 1239. Maximum Length of a Concatenated String with Unique Characters (https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/description/?envType=daily-question&envId=2024-01-23) using DP approach with binary masks

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Maximum_Length_of_a_Concatenated_String_with_Unique_Characters/Solution.java
@@ -0,0 +1,39 @@
+package LeetCode.Daily_Challenge.Medium.Maximum_Length_of_a_Concatenated_String_with_Unique_Characters;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+
+    public int maxLength(List<String> arr) {
+        int n = arr.size();
+        int[] dp = new int[1 << n];
+        int result = 0;
+
+        for (int mask = 0; mask < (1 << n); mask++) {
+            Set<Character> charSet = new HashSet<>();
+            boolean isValid = true;
+            int currentLength = 0;
+
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) > 0) {
+                    for (char c : arr.get(i).toCharArray()) {
+                        if (!charSet.add(c)) {
+                            isValid = false;
+                            break;
+                        }
+                    }
+                    if (!isValid) break;
+                    currentLength += arr.get(i).length();
+                }
+            }
+
+            if (isValid) {
+                dp[mask] = currentLength;
+                result = Math.max(result, dp[mask]);
+            }
+        }
+        return result;
+    }
+}
