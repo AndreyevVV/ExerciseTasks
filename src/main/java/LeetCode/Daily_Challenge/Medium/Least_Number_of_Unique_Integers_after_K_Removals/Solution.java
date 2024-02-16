90cf555956331151e103262aec744274f129fe0e
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-02-16 23:08:01 +0300

    refactoring

--- src/main/java/LeetCode/Daily_Challenge/Medium/Least_Number_of_Unique_Integers_after_K_Removals/Solution.java
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Least_Number_of_Unique_Integers_after_K_Removals/Solution.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().findLeastNumOfUniqueInts(new int[]{4, 3, 1, 1, 3, 3, 2}, 3));
+    }
 
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
         Map<Integer, Integer> map = new HashMap<>();
