bab17049f151c7896d64658961d098a7fed23d50
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-02-16 23:04:38 +0300

    resolved task: 1481. Least Number of Unique Integers after K Removals (https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/description/?envType=daily-question&envId=2024-02-16) using frequency map and priority queue approach

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Least_Number_of_Unique_Integers_after_K_Removals/Solution.java
@@ -0,0 +1,32 @@
+package LeetCode.Daily_Challenge.Medium.Least_Number_of_Unique_Integers_after_K_Removals;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Solution {
+
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) map.put(num, map.getOrDefault(num, 0) + 1);
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.comparingInt(map::get));
+        minHeap.addAll(map.keySet());
+
+        for (int i = 0; i < k; i++) {
+            int element = minHeap.poll();
+            map.put(element, map.get(element) - 1);
+            if (map.get(element) > 0)
+                minHeap.add(element);
+        }
+
+        int uniqueCount = 0;
+        for (int count : map.values())
+            if (count > 0)
+                uniqueCount++;
+
+        return uniqueCount;
+    }
+}
