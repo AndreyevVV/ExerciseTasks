b463581b4b05f7e9cff41e42113acb372d4fba1b
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-02-17 06:22:40 +0300

    refactoring task: 1642. Furthest Building You Can Reach (https://leetcode.com/problems/furthest-building-you-can-reach/description/?envType=daily-question&envId=2024-02-17) with priority queue approach

--- src/main/java/LeetCode/Daily_Challenge/Medium/Furthest_Building_You_Can_Reach/Solution.java
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Furthest_Building_You_Can_Reach/Solution.java
@@ -1,4 +1,4 @@
-package LeetCode.Daily_Challenge.Medium.Furthest_Building_You_Can_Reach.PriorityQueue_solution;
+package LeetCode.Daily_Challenge.Medium.Furthest_Building_You_Can_Reach;
 
 import java.util.PriorityQueue;
 
