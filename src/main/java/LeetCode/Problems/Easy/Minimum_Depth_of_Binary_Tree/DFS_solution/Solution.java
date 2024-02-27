bb7d31ba21fa5d0d021c238bb548fe4fc4cf8d6e
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-02-27 13:06:17 +0300

    resolved task: 144. 111. Minimum Depth of Binary Tree (https://leetcode.com/problems/minimum-depth-of-binary-tree/description/) with iterative DFS solution on Stack

---
+++ src/main/java/LeetCode/Problems/Easy/Minimum_Depth_of_Binary_Tree/DFS_solution/Solution.java
@@ -0,0 +1,67 @@
+package LeetCode.Problems.Easy.Minimum_Depth_of_Binary_Tree.DFS_solution;
+
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().start());
+    }
+
+    private int start() {
+
+        TreeNode seven = new TreeNode(7);
+        TreeNode fifteen = new TreeNode(15);
+        TreeNode twenty = new TreeNode(20, fifteen, seven);
+        TreeNode nine = new TreeNode(9);
+        TreeNode root = new TreeNode(3, nine, twenty);
+
+        return minDepth(root);
+    }
+
+        public int minDepth(TreeNode root) {
+            if (root == null) return 0;
+
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(root);
+            Stack<Integer> depth = new Stack<>();
+            depth.push(1);
+            int minDepth = Integer.MAX_VALUE;
+
+            while (!stack.isEmpty()) {
+                root = stack.pop();
+                int localDepth = depth.pop();
+
+                if (root.left == null && root.right == null)
+                    minDepth = Math.min(minDepth, localDepth);
+
+                if (root.left != null) {
+                    stack.push(root.left);
+                    depth.push(localDepth + 1);
+                }
+                if (root.right != null) {
+                    stack.push(root.right);
+                    depth.push(localDepth + 1);
+                }
+            }
+            return minDepth;
+        }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
