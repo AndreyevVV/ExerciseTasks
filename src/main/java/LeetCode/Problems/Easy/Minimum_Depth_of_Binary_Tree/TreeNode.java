dfef126d9662a1ad1c4c0987ec36b432177da3c5
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-02-27 13:12:19 +0300

    added attachments for task: 144. 111. Minimum Depth of Binary Tree (https://leetcode.com/problems/minimum-depth-of-binary-tree/description/)

---
+++ src/main/java/LeetCode/Problems/Easy/Minimum_Depth_of_Binary_Tree/TreeNode.java
@@ -0,0 +1,21 @@
+package LeetCode.Problems.Easy.Minimum_Depth_of_Binary_Tree;
+
+public class TreeNode {
+    int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+}
