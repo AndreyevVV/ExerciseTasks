661db5b5fb4fb23cd32a48af1a623f7277f9ee7b
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-03-07 14:40:19 +0300

    resolved task: 876. Middle of the Linked List (https://leetcode.com/problems/middle-of-the-linked-list/description/?envType=daily-question&envId=2024-03-07) with two pointers approach

---
+++ src/main/java/LeetCode/LeetCode_75_Level_1/Day_4_Linked_List/Easy/Middle_of_the_Linked_List/Two_pointer_solution/Solution.java
@@ -0,0 +1,32 @@
+package LeetCode.LeetCode_75_Level_1.Day_4_Linked_List.Easy.Middle_of_the_Linked_List.Two_pointer_solution;
+
+public class Solution {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
