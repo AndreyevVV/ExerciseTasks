1e0403bd5441d63571ce73795cd72a0042784b53
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-03-17 21:08:06 +0300

    added solution for task: 57. Insert Interval (https://leetcode.com/problems/insert-interval/description/?envType=daily-question&envId=2024-03-17)

---
+++ src/main/java/LeetCode/LeetCode_75_Level_2/Day_17_Interval/Medium/Insert_Interval/WhileLoop_solution/Solution.java
@@ -0,0 +1,30 @@
+package LeetCode.LeetCode_75_Level_2.Day_17_Interval.Medium.Insert_Interval.WhileLoop_solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval);
+
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
+        }
+        return result.toArray(int[][]::new);
+    }
+}
