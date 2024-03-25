3e19fbaa660808fe54799d8ccb02df3c7f0f960c
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-03-25 22:53:39 +0300

    refactoring

--- src/main/java/LeetCode/Daily_Challenge/Medium/Find_All_Duplicates_in_an_Array/LazyInit_solution/Solution.java
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Find_All_Duplicates_in_an_Array/LazyInit_solution/Solution.java
@@ -6,6 +6,15 @@ import java.util.List;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+
+        Solution solution = new Solution();
+        List<Integer> duplicates = solution.findDuplicates(nums);
+
+        System.out.println("Дубликаты в массиве nums: " + duplicates);
+    }
+
     private List<Integer> res;
 
     public List<Integer> findDuplicates(int[] nums) {
