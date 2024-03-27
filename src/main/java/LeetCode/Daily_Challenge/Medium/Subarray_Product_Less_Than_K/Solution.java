35f03c9251834a45fd70a3c0eac41de07a3dd536
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-03-27 22:45:06 +0300

    resolved task: 713. Subarray Product Less Than K (https://leetcode.com/problems/subarray-product-less-than-k/description/?envType=daily-question&envId=2024-03-27)

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Subarray_Product_Less_Than_K/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.Daily_Challenge.Medium.Subarray_Product_Less_Than_K;
+
+public class Solution {
+
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+
+        int count = 0;
+        int product = 1;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            product *= nums[right];
+            while (product >= k) {
+                product /= nums[left];
+                left++;
+            }
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+}
