55d910eb1e2efa7286dcef8586e5b81c85fface8
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-04-03 23:36:30 +0300

    resolved task: 1863. Sum of All Subset XOR Totals (https://leetcode.com/problems/sum-of-all-subset-xor-totals/description/) with backtracking approach

---
+++ src/main/java/LeetCode/Problems/Easy/Sum_of_All_Subset_XOR_Totals/Solution.java
@@ -0,0 +1,18 @@
+package LeetCode.Problems.Easy.Sum_of_All_Subset_XOR_Totals;
+
+public class Solution {
+
+    public int subsetXORSum(int[] nums) {
+        return backtrack(nums, 0, 0);
+    }
+
+    private int backtrack(int[] nums, int index, int xorSum) {
+        if (index == nums.length)
+            return xorSum;
+
+        int include = backtrack(nums, index + 1, xorSum ^ nums[index]);
+        int exclude = backtrack(nums, index + 1, xorSum);
+
+        return include + exclude;
+    }
+}
