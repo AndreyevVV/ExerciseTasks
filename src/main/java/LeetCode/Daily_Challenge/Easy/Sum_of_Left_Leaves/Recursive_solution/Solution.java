2de5536c4a6985ff45ea542e9c81614d9d30d109
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-04-14 21:57:26 +0300

    resolved task: 404. Sum of Left Leaves (https://leetcode.com/problems/sum-of-left-leaves/description/?envType=daily-question&envId=2024-04-14) with recursive

---
+++ src/main/java/LeetCode/Daily_Challenge/Easy/Sum_of_Left_Leaves/Recursive_solution/Solution.java
@@ -0,0 +1,25 @@
+package LeetCode.Daily_Challenge.Easy.Sum_of_Left_Leaves.Recursive_solution;
+
+import LeetCode.Daily_Challenge.Easy.Sum_of_Left_Leaves.TreeNode;
+
+public class Solution {
+
+    public int left = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+
+        class Traverse {
+            Traverse(TreeNode root) {
+                TreeNode temp = root;
+                if (temp.left != null) {
+                    if (temp.left.left == null && temp.left.right == null) left += temp.left.val;
+                    new Traverse(temp.left);
+                }
+                if (temp.right != null)
+                    new Traverse(temp.right);
+            }
+        }
+        new Traverse(root);
+        return left;
+    }
+}
