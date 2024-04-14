dae2c566e732d2d58ae62f5ef3d24e52cd63c0d1
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-04-14 21:57:47 +0300

    refactoring task: 404. Sum of Left Leaves (https://leetcode.com/problems/sum-of-left-leaves/description/?envType=daily-question&envId=2024-04-14)

--- src/main/java/LeetCode/Daily_Challenge/Easy/Sum_of_Left_Leaves/Iterative_solution/Solution.java
+++ src/main/java/LeetCode/Daily_Challenge/Easy/Sum_of_Left_Leaves/Iterative_solution/Solution.java
@@ -1,5 +1,7 @@
 package LeetCode.Daily_Challenge.Easy.Sum_of_Left_Leaves.Iterative_solution;
 
+import LeetCode.Daily_Challenge.Easy.Sum_of_Left_Leaves.TreeNode;
+
 import java.util.Stack;
 
 public class Solution {
@@ -21,32 +23,5 @@ public class Solution {
             root = root.right;
         }
         return leftSum;
-    }
-
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-
-        @Override
-        public String toString() {
-            return "{" + val +
-                    " " + left +
-                    " " + right +
-                    '}';
-        }
     }
 }
