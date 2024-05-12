375b086f32631ed51219ae96dcaec114f3b9714f
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-05-12 23:37:43 +0300

    resolved task: 303. Range Sum Query - Immutable (https://leetcode.com/problems/range-sum-query-immutable/description/) with PrefixSum approach

---
+++ src/main/java/LeetCode/Problems/Easy/Range_Sum_Query_Immutable/Solution.java
@@ -0,0 +1,19 @@
+package LeetCode.Problems.Easy.Range_Sum_Query_Immutable;
+
+public class Solution {
+    class NumArray {
+        private int[] prefixSum;
+
+        public NumArray(int[] nums) {
+            int n = nums.length;
+            prefixSum = new int[n + 1];
+            for (int i = 0; i < n; i++) {
+                prefixSum[i + 1] = prefixSum[i] + nums[i];
+            }
+        }
+
+        public int sumRange(int left, int right) {
+            return prefixSum[right + 1] - prefixSum[left];
+        }
+    }
+}
