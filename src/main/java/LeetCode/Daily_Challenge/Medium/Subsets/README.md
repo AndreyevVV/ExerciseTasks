75768f3da41e00eb18689337f8bafd12cd9f39c0
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-05-21 23:00:56 +0300

    resolved task: 78. Subsets (https://leetcode.com/problems/subsets/description/?envType=daily-question&envId=2024-05-21) with BackTracking approach

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Subsets/README.md
@@ -0,0 +1,30 @@
+---
+created: 2024-05-21T22:54:33 (UTC +03:00)
+source: https://leetcode.com/problems/subsets/description/?envType=daily-question&envId=2024-05-21
+---
+Given an integer array `nums` of **unique** elements, return _all possible subsets (the power set)_.
+
+The solution set **must not** contain duplicate subsets. Return the solution in **any order**.
+
+
+**Example 1:**
+
+``` Java
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+```
+
+
+**Example 2:**
+
+``` Java
+Input: nums = [0]
+Output: [[],[0]]
+```
+
+
+**Constraints:**
+
+* `1 <= nums.length <= 10`
+* `-10 <= nums[i] <= 10`
+* All the numbers of `nums` are **unique**.
---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Subsets/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.Daily_Challenge.Medium.Subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        backtrack(subsets, new ArrayList<>(), nums, 0);
+        return subsets;
+    }
+
+    private void backtrack(List<List<Integer>> subsets, List<Integer> currentSubset, int[] nums, int start) {
+        subsets.add(new ArrayList<>(currentSubset));
+
+        for (int i = start; i < nums.length; i++) {
+            currentSubset.add(nums[i]);
+            backtrack(subsets, currentSubset, nums, i + 1);
+            currentSubset.remove(currentSubset.size() - 1);
+        }
+    }
+}
