655abd456c0b2f275f1765dfcfce3f45a6356547
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-05-22 23:08:53 +0300

    resolved task: 132. Palindrome Partitioning II (https://leetcode.com/problems/palindrome-partitioning-ii/description/) with DP approach

---
+++ src/main/java/LeetCode/Problems/Hard/Palindrome_Partitioning_II/README.md
@@ -0,0 +1,40 @@
+---
+created: 2024-05-22T22:56:25 (UTC +03:00)
+source: https://leetcode.com/problems/palindrome-partitioning-ii/description/
+---
+Given a string `s`, partition `s` such that every
+
+of the partition is a .
+
+Return _the **minimum** cuts needed for a palindrome partitioning of_ `s`.
+
+
+**Example 1:**
+
+``` Java
+Input: s = "aab"
+Output: 1
+Explanation: The palindrome partitioning ["aa","b"] could be produced using 1 cut.
+```
+
+
+**Example 2:**
+
+``` Java
+Input: s = "a"
+Output: 0
+```
+
+
+**Example 3:**
+
+``` Java
+Input: s = "ab"
+Output: 1
+```
+
+
+**Constraints:**
+
+-   `1 <= s.length <= 2000`
+-   `s` consists of lowercase English letters only.
---
+++ src/main/java/LeetCode/Problems/Hard/Palindrome_Partitioning_II/Solution.java
@@ -0,0 +1,28 @@
+package LeetCode.Problems.Hard.Palindrome_Partitioning_II;
+
+public class Solution {
+
+    public int minCut(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+
+        int[] dp = new int[n];
+        boolean[][] palindrome = new boolean[n][n];
+
+        for (int i = 0; i < n; i++)
+            dp[i] = i;
+
+        for (int end = 0; end < n; end++) {
+            for (int start = 0; start <= end; start++) {
+                if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || palindrome[start + 1][end - 1])) {
+                    palindrome[start][end] = true;
+                    if (start == 0)
+                        dp[end] = 0;
+                    else
+                        dp[end] = Math.min(dp[end], dp[start - 1] + 1);
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+}
