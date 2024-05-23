44aaa3a66b81e0c41ce7e03eda77a3bc5d24c237
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-05-23 20:06:46 +0300

    resolved task: 2597. The Number of Beautiful Subsets (https://leetcode.com/problems/the-number-of-beautiful-subsets/description/?envType=daily-question&envId=2024-05-23) with BackTracking approach

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/The_Number_of_Beautiful_Subsets/Solution.java
@@ -0,0 +1,38 @@
+package LeetCode.Daily_Challenge.Medium.The_Number_of_Beautiful_Subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public int count = 0;
+
+    public int beautifulSubsets(int[] nums, int k) {
+        countBeautifulSubsets(nums, k, 0, new ArrayList<>());
+        return count - 1;
+    }
+
+    private void countBeautifulSubsets(int[] nums, int k, int index, List<Integer> currentSubset) {
+        if (index >= nums.length) {
+            count++;
+            return;
+        }
+
+        countBeautifulSubsets(nums, k, index + 1, currentSubset);
+
+        int currentElement = nums[index];
+        boolean isBeautiful = true;
+        for (int num : currentSubset) {
+            if (Math.abs(num - currentElement) == k) {
+                isBeautiful = false;
+                break;
+            }
+        }
+
+        if (isBeautiful) {
+            currentSubset.add(currentElement);
+            countBeautifulSubsets(nums, k, index + 1, currentSubset);
+            currentSubset.remove(currentSubset.size() - 1);
+        }
+    }
+}
