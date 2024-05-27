5e0bab3dea66e16f0a8d765472ff42c52a965d46
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-05-27 22:20:52 +0300

    resolved task: 1608. Special Array With X Elements Greater Than or Equal X (https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/description/?envType=daily-question&envId=2024-05-27)

---
+++ src/main/java/LeetCode/Daily_Challenge/Easy/Special_Array_With_X_Elements_Greater_Than_or_Equal_X/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.Daily_Challenge.Easy.Special_Array_With_X_Elements_Greater_Than_or_Equal_X;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for (int x = 0; x <= n; x++) {
+            int count = 0;
+            for (int num : nums)
+                if (num >= x)
+                    count++;
+            if (count == x)
+                return x;
+        }
+
+        return -1;
+    }
+}
