956a7b9882ca8663e0173fa84b8ed7ef9b7df420
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-06-17 20:35:10 +0300

    resolved task: 330. Patching Array (https://leetcode.com/problems/patching-array/description/?envType=daily-question&envId=2024-06-16)

--- src/main/java/LeetCode/Daily_Challenge/Hard/Patching_Array/Solution.java
+++ src/main/java/LeetCode/Daily_Challenge/Hard/Patching_Array/Solution.java
@@ -3,9 +3,20 @@ package LeetCode.Daily_Challenge.Hard.Patching_Array;
 public class Solution {
 
     public int minPatches(int[] nums, int n) {
+        int patches = 0;
+        long miss = 1;
+        int i = 0;
 
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i];
+                i++;
+            } else {
+                miss += miss;
+                patches++;
+            }
+        }
 
-
-        return -1;
+        return patches;
     }
 }
