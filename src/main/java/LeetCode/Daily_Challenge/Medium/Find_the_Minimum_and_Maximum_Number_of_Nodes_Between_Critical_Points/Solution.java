8bd6ae1970ed6f19ed1a33377675df20218035ab
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-07-05 20:39:14 +0300

    refactoring

--- src/main/java/LeetCode/Daily_Challenge/Medium/Find_the_Minimum_and_Maximum_Number_of_Nodes_Between_Critical_Points/Solution.java
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Find_the_Minimum_and_Maximum_Number_of_Nodes_Between_Critical_Points/Solution.java
@@ -47,9 +47,8 @@ public class Solution {
     }
 
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        if (head == null || head.next == null || head.next.next == null) {
+        if (head == null || head.next == null || head.next.next == null)
             return new int[]{-1, -1};
-        }
 
         int firstCriticalIndex = -1;
         int lastCriticalIndex = -1;
@@ -61,11 +60,11 @@ public class Solution {
         while (curr.next != null) {
             if ((curr.val > prev.val && curr.val > curr.next.val) || 
                 (curr.val < prev.val && curr.val < curr.next.val)) {
-                if (firstCriticalIndex == -1) {
+                if (firstCriticalIndex == -1)
                     firstCriticalIndex = index;
-                } else {
+                else
                     minDistance = Math.min(minDistance, index - lastCriticalIndex);
-                }
+            
                 lastCriticalIndex = index;
             }
             prev = curr;
@@ -73,9 +72,9 @@ public class Solution {
             index++;
         }
 
-        if (firstCriticalIndex == lastCriticalIndex) {
+        if (firstCriticalIndex == lastCriticalIndex)
             return new int[]{-1, -1};
-        }
+    
 
         int maxDistance = lastCriticalIndex - firstCriticalIndex;
         return new int[]{minDistance, maxDistance};
