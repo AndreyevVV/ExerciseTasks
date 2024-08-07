e932d12158f2760d85bc4a72b56c18c08ba40114
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-08-07 23:19:14 +0300

    resolved task: 273. Integer to English Words

---
+++ src/main/java/LeetCode/Daily_Challenge/Hard/Integer_to_English_Words/README.md
@@ -0,0 +1,34 @@
+---
+created: 2024-08-07T23:13:56 (UTC +03:00)
+source: https://leetcode.com/problems/integer-to-english-words/description/?envType=daily-question&envId=2024-08-07
+---
+Convert a non-negative integer `num` to its English words representation.
+
+
+**Example 1:**
+
+``` Java
+Input: num = 123
+Output: "One Hundred Twenty Three"
+```
+
+
+**Example 2:**
+
+``` Java
+Input: num = 12345
+Output: "Twelve Thousand Three Hundred Forty Five"
+```
+
+
+**Example 3:**
+
+``` Java
+Input: num = 1234567
+Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+```
+
+
+**Constraints:**
+
+-   `0 <= num <= 2^31 - 1`
---
+++ src/main/java/LeetCode/Daily_Challenge/Hard/Integer_to_English_Words/Solution.java
@@ -0,0 +1,42 @@
+package LeetCode.Daily_Challenge.Hard.Integer_to_English_Words;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution converter = new Solution();
+        System.out.println(converter.numberToWords(123));         // Output: One Hundred Twenty Three
+        System.out.println(converter.numberToWords(12345));       // Output: Twelve Thousand Three Hundred Forty Five
+        System.out.println(converter.numberToWords(1234567));     // Output: One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven
+    }
+
+    private static final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String[] TENS = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+
+
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+
+        int i = 0;
+        String words = "";
+
+        while (num > 0) {
+            if (num % 1000 != 0)
+                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+            num /= 1000;
+            i++;
+        }
+
+        return words.trim();
+    }
+
+    private String helper(int num) {
+        if (num == 0)
+            return "";
+        else if (num < 20)
+            return LESS_THAN_20[num] + " ";
+        else if (num < 100)
+            return TENS[num / 10] + " " + helper(num % 10);
+        else
+            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+    }
+}
