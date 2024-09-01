25c2d38eb7c9d86784490bd8d67db2f8ecd0f865
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-09-01 14:24:10 +0300

    resolved task: 2022. Convert 1D Array Into 2D Array (https://leetcode.com/problems/convert-1d-array-into-2d-array/description/?envType=daily-question&envId=2024-09-01)

---
+++ src/main/java/LeetCode/Daily_Challenge/Easy/Convert_1D_Array_Into_2D_Array/Solution.java
@@ -0,0 +1,16 @@
+package LeetCode.Daily_Challenge.Easy.Convert_1D_Array_Into_2D_Array;
+
+public class Solution {
+
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (original.length != m * n)
+            return new int[0][0];
+
+        int[][] result = new int[m][n];
+
+        for (int i = 0; i < original.length; i++)
+            result[i / n][i % n] = original[i];
+
+        return result;
+    }
+}
