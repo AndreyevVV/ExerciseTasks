4de21f2ac3e84caf59b5832812393f7d68b181d4
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-09-26 23:27:12 +0300

    resolved task: 729. My Calendar I (https://leetcode.com/problems/my-calendar-i/description/?envType=daily-question&envId=2024-09-26)

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/My_Calendar_I/Solution.java
@@ -0,0 +1,30 @@
+package LeetCode.Daily_Challenge.Medium.My_Calendar_I;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    class MyCalendar {
+        private List<int[]> calendar;
+
+        public MyCalendar() {
+            calendar = new ArrayList<>();
+        }
+
+        public boolean book(int start, int end) {
+            for (int[] event : calendar)
+                if (Math.max(event[0], start) < Math.min(event[1], end))
+                    return false;
+
+            calendar.add(new int[]{start, end});
+            return true;
+        }
+    }
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
+}
