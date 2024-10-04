05dd536c814a8373eb91e52fb375c9e2fb61ede1
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-10-04 08:58:21 +0300

    resolved task: 2491. Divide Players Into Teams of Equal Skill (https://leetcode.com/problems/divide-players-into-teams-of-equal-skill/description/?envType=daily-question&envId=2024-10-04)

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Divide_Players_Into_Teams_of_Equal_Skill/Solution.java
@@ -0,0 +1,21 @@
+package LeetCode.Daily_Challenge.Medium.Divide_Players_Into_Teams_of_Equal_Skill;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public long dividePlayers(int[] skill) {
+        Arrays.sort(skill);
+        int totalSkill = skill[0] + skill[skill.length - 1];
+        long chemistry = 0;
+
+        for (int i = 0, j = skill.length - 1; i < j; i++, j--) {
+            if (skill[i] + skill[j] != totalSkill) return -1L;
+            chemistry += (long) skill[i] * skill[j];
+        }
+
+        return chemistry;
+    }
+}
