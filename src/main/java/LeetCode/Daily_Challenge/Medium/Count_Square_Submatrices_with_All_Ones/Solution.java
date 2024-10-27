51cff8d3232359afb225dfb1f6907a7c37f8860a
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-10-27 23:32:45 +0300

    resolved task: 1277. Count Square Submatrices with All Ones (https://leetcode.com/problems/count-square-submatrices-with-all-ones/description/?envType=daily-question&envId=2024-10-27) with DP approach

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Count_Square_Submatrices_with_All_Ones/Solution.java
@@ -0,0 +1,25 @@
+package LeetCode.Daily_Challenge.Medium.Count_Square_Submatrices_with_All_Ones;
+
+public class Solution {
+
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0)
+                        dp[i][j] = 1;
+                    else
+                        dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+                    count += dp[i][j];
+                }
+            }
+        }
+
+        return count;
+    }
+}
