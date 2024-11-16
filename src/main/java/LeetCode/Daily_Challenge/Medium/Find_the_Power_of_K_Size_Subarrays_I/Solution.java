0952da60758e52f7c1e31d369d0e15508cf77956
Andreev Vladimir <andreyevpostbox@gmail.com>
2024-11-16 23:23:54 +0300

    resolved task: 3254. Find the Power of K-Size Subarrays I (https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i/?envType=daily-question&envId=2024-11-16)

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Find_the_Power_of_K_Size_Subarrays_I/Solution.java
@@ -0,0 +1,27 @@
+package LeetCode.Daily_Challenge.Medium.Find_the_Power_of_K_Size_Subarrays_I;
+
+public class Solution {
+
+    public int[] resultsArray(int[] nums, int k) {
+        int n = nums.length;
+        int[] results = new int[n - k + 1];
+
+        for (int i = 0; i <= n - k; i++) {
+            boolean isValid = true;
+
+            for (int j = i; j < i + k - 1; j++) {
+                if (nums[j + 1] - nums[j] != 1) {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid)
+                results[i] = nums[i + k - 1];
+            else
+                results[i] = -1;
+        }
+
+        return results;
+    }
+}
