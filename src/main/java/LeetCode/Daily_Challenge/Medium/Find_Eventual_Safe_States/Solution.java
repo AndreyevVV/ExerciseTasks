de3609e03a5c383504b15711220b6c159b73f5d5
Andreev Vladimir <andreyevpostbox@gmail.com>
2025-01-24 05:37:02 +0300

    resolved task: 802. Find Eventual Safe States (https://leetcode.com/problems/find-eventual-safe-states/description/?envType=daily-question&envId=2025-01-24)

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Find_Eventual_Safe_States/Solution.java
@@ -0,0 +1,41 @@
+package LeetCode.Daily_Challenge.Medium.Find_Eventual_Safe_States;
+
+import java.util.*;
+
+public class Solution {
+
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        List<List<Integer>> reverseGraph = new ArrayList<>();
+        int[] inDegree = new int[n];
+
+        for (int i = 0; i < n; i++)
+            reverseGraph.add(new ArrayList<>());
+
+        for (int i = 0; i < n; i++) {
+            for (int neighbor : graph[i]) {
+                reverseGraph.get(neighbor).add(i);
+                inDegree[i]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++)
+            if (inDegree[i] == 0)
+                queue.offer(i);
+
+        List<Integer> safeNodes = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            safeNodes.add(node);
+            for (int neighbor : reverseGraph.get(node)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0)
+                    queue.offer(neighbor);
+            }
+        }
+
+        Collections.sort(safeNodes);
+        return safeNodes;
+    }
+}
