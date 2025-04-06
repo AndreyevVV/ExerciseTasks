12e33da6b4dd2f6aaea688002cf0585b71c782c0
Andreev Vladimir <andreyevpostbox@gmail.com>
2025-04-06 10:52:11 +0300

    resolved task: 368. Largest Divisible Subset (https://leetcode.com/problems/largest-divisible-subset/description/?envType=daily-question&envId=2025-04-06)

--- src/main/java/LeetCode/Daily_Challenge/Medium/Largest_Divisible_Subset/DP_solution/Solution.java
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Largest_Divisible_Subset/DP_solution/Solution.java
@@ -2,11 +2,12 @@ package LeetCode.Daily_Challenge.Medium.Largest_Divisible_Subset.DP_solution;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(new Solution().largestDivisibleSubset(new int[]{1,2,4,8}));
+        System.out.println(new Solution().largestDivisibleSubset(new int[]{1, 2, 4, 8}));
     }
 
     public List<Integer> largestDivisibleSubset(int[] nums) {
@@ -15,33 +16,35 @@ public class Solution {
 
         Arrays.sort(nums);
         int n = nums.length;
+        if (n == 0) return new ArrayList<>();
+
+        Arrays.sort(nums);
+
         int[] dp = new int[n];
+        int[] prev = new int[n];
         Arrays.fill(dp, 1);
-        int maxSize = 1, maxIndex = 0;
+        Arrays.fill(prev, -1);
+
+        int maxIndex = 0;
 
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
                     dp[i] = dp[j] + 1;
-                    if (dp[i] > maxSize) {
-                        maxSize = dp[i];
-                        maxIndex = i;
-                    }
+                    prev[i] = j;
                 }
             }
+            if (dp[i] > dp[maxIndex])
+                maxIndex = i;
         }
 
         List<Integer> result = new ArrayList<>();
-        int currentSize = maxSize;
-        int currentNum = nums[maxIndex];
-
-        for (int i = maxIndex; i >= 0; i--) {
-            if (currentSize > 0 && currentNum % nums[i] == 0 && dp[i] == currentSize) {
-                result.add(nums[i]);
-                currentNum = nums[i];
-                currentSize--;
-            }
+        for (int i = maxIndex; i >= 0; i = prev[i]) {
+            result.add(nums[i]);
+            if (prev[i] == -1) break;
         }
+
+        Collections.reverse(result);
         return result;
     }
 }
