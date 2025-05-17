24926c0fe84760d0dc068f0d5e59cdc237a14220
Andreev Vladimir <andreyevpostbox@gmail.com>
2025-05-17 06:42:59 +0300

    refactoring task: 75. Sort Colors (https://leetcode.com/problems/sort-colors/description/?envType=daily-question&envId=2025-05-17)

--- src/main/java/LeetCode/Daily_Challenge/Medium/Sort_Colors/Solution.java
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Sort_Colors/Solution.java
@@ -3,28 +3,22 @@ package LeetCode.Daily_Challenge.Medium.Sort_Colors;
 public class Solution {
 
     public void sortColors(int[] nums) {
-        int low = 0;
-        int mid = 0;
-        int high = nums.length - 1;
+        int low = 0, mid = 0, high = nums.length - 1;
 
         while (mid <= high) {
-            switch (nums[mid]) {
-                case 0:
-                    int temp0 = nums[low];
-                    nums[low] = nums[mid];
-                    nums[mid] = temp0;
-                    low++;
-                    mid++;
-                    break;
-                case 1:
-                    mid++;
-                    break;
-                case 2:
-                    int temp2 = nums[high];
-                    nums[high] = nums[mid];
-                    nums[mid] = temp2;
-                    high--;
-                    break;
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
             }
         }
     }
