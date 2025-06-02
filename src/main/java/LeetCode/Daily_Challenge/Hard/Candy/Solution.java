d292d46f0f27a114b93a50b75cc1b8b5b2b8db1a
Andreev Vladimir <andreyevpostbox@gmail.com>
2025-06-02 21:35:12 +0300

    resolved task: 135. Candy (https://leetcode.com/problems/candy/description/?envType=daily-question&envId=2025-06-02)

---
+++ src/main/java/LeetCode/Daily_Challenge/Hard/Candy/Solution.java
@@ -0,0 +1,25 @@
+package LeetCode.Daily_Challenge.Hard.Candy;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) return 0;
+
+        int length = ratings.length;
+        int[] candies = new int[length];
+
+        for (int i = 1; i < length; i++) {
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        }
+
+        for (int i = length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1])
+                if (candies[i] <= candies[i + 1])
+                    candies[i] = candies[i + 1] + 1;
+        }
+        return Arrays.stream(candies).reduce(Integer::sum).getAsInt() + ratings.length;
+    }
+}
