78408c27490821146f284686eee73fda49f3aaaa
Andreev Vladimir <andreyevpostbox@gmail.com>
2025-06-05 15:55:11 +0300

    resolved task: 1061. Lexicographically Smallest Equivalent String (https://leetcode.com/problems/lexicographically-smallest-equivalent-string/?envType=daily-question&envId=2025-06-05)

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Lexicographically_Smallest_Equivalent_String/Solution.java
@@ -0,0 +1,36 @@
+package LeetCode.Daily_Challenge.Medium.Lexicographically_Smallest_Equivalent_String;
+
+public class Solution {
+
+    private int[] parent = new int[26];
+
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        for (int i = 0; i < 26; i++) parent[i] = i;
+
+        for (int i = 0; i < s1.length(); i++)
+            union(s1.charAt(i) - 'a', s2.charAt(i) - 'a');
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : baseStr.toCharArray())
+            sb.append((char) (find(c - 'a') + 'a'));
+
+        return sb.toString();
+    }
+
+    private int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+
+        return parent[x];
+    }
+
+    private void union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px == py) return;
+        if (px < py)
+            parent[py] = px;
+        else
+            parent[px] = py;
+    }
+}
