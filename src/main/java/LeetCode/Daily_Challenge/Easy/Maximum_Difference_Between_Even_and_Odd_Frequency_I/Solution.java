9949816bb8f8f5c47379463646cd31319c9d13df
Andreev Vladimir <andreyevpostbox@gmail.com>
2025-06-10 18:38:54 +0300

    resolved task: 3442. Maximum Difference Between Even and Odd Frequency I (https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/description/?envType=daily-question&envId=2025-06-10)

---
+++ src/main/java/LeetCode/Daily_Challenge/Easy/Maximum_Difference_Between_Even_and_Odd_Frequency_I/Solution.java
@@ -0,0 +1,21 @@
+package LeetCode.Daily_Challenge.Easy.Maximum_Difference_Between_Even_and_Odd_Frequency_I;
+
+public class Solution {
+
+    public int maxDifference(String s) {
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray())
+            freq[c - 'a']++;
+
+        int maxDiff = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 26; i++)
+            if (freq[i] % 2 == 1)
+                for (int j = 0; j < 26; j++)
+                    if (freq[j] > 0 && freq[j] % 2 == 0)
+                        maxDiff = Math.max(maxDiff, freq[i] - freq[j]);
+
+        return maxDiff == Integer.MIN_VALUE ? 0 : maxDiff;
+    }
+}
