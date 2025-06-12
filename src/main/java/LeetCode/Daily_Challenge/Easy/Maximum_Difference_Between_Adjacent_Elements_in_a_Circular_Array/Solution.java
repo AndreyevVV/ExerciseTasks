03e701bf74804f66533cee9f368874035a317473
Andreev Vladimir <andreyevpostbox@gmail.com>
2025-06-12 03:37:01 +0300

    resolved task: 3423. Maximum Difference Between Adjacent Elements in a Circular Array (https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/description/?envType=daily-question&envId=2025-06-12)

---
+++ src/main/java/LeetCode/Daily_Challenge/Easy/Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array/Solution.java
@@ -0,0 +1,17 @@
+package LeetCode.Daily_Challenge.Easy.Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array;
+
+public class Solution {
+
+    public int maxAdjacentDistance(int[] nums) {
+        int maxDiff = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int nextIndex = (i + 1) % n;
+            int diff = Math.abs(nums[i] - nums[nextIndex]);
+            maxDiff = Math.max(maxDiff, diff);
+        }
+
+        return maxDiff;
+    }
+}
