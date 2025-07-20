97984795b07a1e0e93e5fba91c26740519e90c77
Andreev Vladimir <andreyevpostbox@gmail.com>
2025-07-20 22:11:37 +0300

    resolved task: 1948. Delete Duplicate Folders in System (https://leetcode.com/problems/delete-duplicate-folders-in-system/description/?envType=daily-question&envId=2025-07-20)

---
+++ src/main/java/LeetCode/Daily_Challenge/Hard/Delete_Duplicate_Folders_in_System/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.Daily_Challenge.Hard.Delete_Duplicate_Folders_in_System;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<List<String>> deleteDuplicateFolder(List<List<String>> paths) {
+        
+
+
+        return new ArrayList<>();
+    }
+}
