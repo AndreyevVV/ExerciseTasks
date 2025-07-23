9c0944277eb01bc876e053086a374491f18ffcb9
Andreev Vladimir <andreyevpostbox@gmail.com>
2025-07-23 21:28:44 +0300

    resolved task: 1717. Maximum Score From Removing Substrings (https://leetcode.com/problems/maximum-score-from-removing-substrings/description/?envType=daily-question&envId=2025-07-23)

---
+++ src/main/java/LeetCode/Daily_Challenge/Medium/Maximum_Score_From_Removing_Substrings/Alter_Solution/Solution.java
@@ -0,0 +1,39 @@
+package LeetCode.Daily_Challenge.Medium.Maximum_Score_From_Removing_Substrings.Alter_Solution;
+
+public class Solution {
+    
+    public int maximumGain(String s, int x, int y) {
+        if (x >= y) {
+            Result r1 = removePairs(s, 'a', 'b', x);
+            Result r2 = removePairs(r1.str, 'b', 'a', y);
+            return r1.score + r2.score;
+        } else {
+            Result r1 = removePairs(s, 'b', 'a', y);
+            Result r2 = removePairs(r1.str, 'a', 'b', x);
+            return r1.score + r2.score;
+        }
+    }
+
+    private Result removePairs(String s, char first, char second, int points) {
+        StringBuilder sb = new StringBuilder();
+        int score = 0;
+        for (char c : s.toCharArray()) {
+            sb.append(c);
+            int len = sb.length();
+            if (len >= 2 && sb.charAt(len - 2) == first && sb.charAt(len - 1) == second) {
+                sb.delete(sb.length() - 2, sb.length());
+                score += points;
+            }
+        }
+        return new Result(sb.toString(), score);
+    }
+
+    private static class Result {
+        String str;
+        int score;
+        Result(String str, int score) {
+            this.str = str;
+            this.score = score;
+        }
+    }
+}
